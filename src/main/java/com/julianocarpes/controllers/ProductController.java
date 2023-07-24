93c23de03ec5aad37cf18e3d7b64c163fda83998
JulianoCarpes <julianocarpes.dev@gmail.com>
2023-07-24 16:35:38 -0300

    corrected control methods and implemented delete

--- src/main/java/com/julianocarpes/controllers/ProductController.java
+++ src/main/java/com/julianocarpes/controllers/ProductController.java
@@ -3,8 +3,14 @@ package com.julianocarpes.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,16 +20,33 @@ import com.julianocarpes.services.ProductService;
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
-	
+
 	@Autowired
 	private ProductService service;
+
 	@GetMapping
-	private List<Product> findAll(){
+	private List<Product> findAll() {
 		return service.findAll();
 	}
+
 	@GetMapping(value = "/{id}")
-	private Product findById(@PathVariable(value = "id")Long id) {
+	private Product findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	private Product created(@RequestBody Product prod) {
+		return service.created(prod);
+	}
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	private Product update(@RequestBody Product prod) {
+		return service.update(prod);
+	}
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id")Long id ) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
--- src/main/java/com/julianocarpes/services/ProductService.java
+++ src/main/java/com/julianocarpes/services/ProductService.java
@@ -6,10 +6,9 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.julianocarpes.exceptions.MyExceptions;
+import com.julianocarpes.exceptions.ResourceNotFaundExceptions;
 import com.julianocarpes.models.Product;
 import com.julianocarpes.repositories.ProductRepository;
-import com.julianocarpes.utils.Calculation;
 
 @Service
 public class ProductService {
@@ -21,16 +20,34 @@ public class ProductService {
 	
 	public List<Product>findAll(){
 		logger.info("Finding a Porducts");
-		List<Product> products = repository.findAll();
-		for (Product product : products) {
-			product.setAmount(Calculation.calculateAmount(product.getPrice(), product.getQuantity()));
-		}
-		return products;
+		return repository.findAll();
+	
 	}
 	
 	public Product findById(Long id) {
 		logger.info("Finding a Product for id");
-		return repository.findById(id).orElseThrow(()-> new MyExceptions("No Records found for this ID"));
+		return repository.findById(id).orElseThrow(()-> new ResourceNotFaundExceptions("No Records found for this ID"));
+	}
+	
+	public Product created(Product product) {
+		logger.info("Created a person");
+		return repository.save(product);
+	}
+	
+	public Product update(Product prod) {
+		Product entity = repository.findById(prod.getId()).orElseThrow(()-> new ResourceNotFaundExceptions("No Records found for this ID"));
+		entity.setName(prod.getName());
+		entity.setQuantity(prod.getQuantity());
+		entity.setPrice(prod.getPrice());
+		entity.setAmount(prod.getAmount());
+		entity.setDescription(prod.getDescription());
+		return repository.save(prod);
+		
+	}
+	public void delete(Long id) {
+		logger.info("Delete a person");
+		Product entity = repository.findById(id).orElseThrow(()-> new ResourceNotFaundExceptions("No Records found for this ID"));
+		repository.delete(entity);
 	}
 
 }
